d2b32f7196ce743c92e32babc3fd3e0d2a0950b2
pockerman <a.giavaras@gmail.com>
2023-01-25 13:11:10 +0000

    Update setup script

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    name='awsutils',
-    packages=['awsutils'],
-    version='0.0.3',
+    name='navalmartin_mir_db_utils',
+    packages=['navalmartin_mir_db_utils'],
+    version='0.0.1',
     description='Various utilities for working with AWS in Python 3.',
     author='Alexandros Giavaras',
     author_email='a.giavaras@gmail.com',
-    url='https://github.com/pockerman/awsutils',
+    url='https://github.com/Navalmartin/mir_persistence_layer_utils',
     download_url='https://github.com/pockerman/awsutils/awsutils-0.0.3.tar.gz',
-    keywords=['aws', 'aws-utilities'],
+    keywords=['mongodb', 'mongodb-utilities'],
     classifiers=[],
     scripts=[],
 )
