7f6a44316f8bac00fc7c9e7531ef9ac3a0eb3494
pockerman <a.giavaras@gmail.com>
2023-01-25 13:05:37 +0000

    Add setup scripts

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='awsutils',
+    packages=['awsutils'],
+    version='0.0.3',
+    description='Various utilities for working with AWS in Python 3.',
+    author='Alexandros Giavaras',
+    author_email='a.giavaras@gmail.com',
+    url='https://github.com/pockerman/awsutils',
+    download_url='https://github.com/pockerman/awsutils/awsutils-0.0.3.tar.gz',
+    keywords=['aws', 'aws-utilities'],
+    classifiers=[],
+    scripts=[],
+)
