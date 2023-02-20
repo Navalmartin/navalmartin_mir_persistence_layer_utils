2f6fb33b20a2a26add14fbdcbf1871072b3eea0e
pockerman <a.giavaras@gmail.com>
2023-02-20 14:45:56 +0000

    Add is_valid_object_id utility

--- src/navalmartin_mir_db_utils/dbs/dbs_utils.py
+++ src/navalmartin_mir_db_utils/dbs/dbs_utils.py
@@ -4,3 +4,24 @@ INVALID_BSON_ID = bson.ObjectId()
 DB_INFO = "DB_INFO: "
 DB_WARNING = "DB_WARNING: "
 DB_ERROR = "DB_ERROR: "
+
+from navalmartin_mir_db_utils.utils.exceptions import InvalidObjectIdException
+
+def is_valid_object_id(oid: str) -> bson.ObjectId:
+    """Attempts to change the given oid in a valid bson.ObjectId.
+    It raises InvalidObjectIdException if the cast fials
+    Paramaters
+    ----------
+    oid: The object id to check
+
+    Returns
+    -------
+
+    An instance of bson.ObjectId upon successfully casting the oid in 
+    a bson.ObjectId
+    """
+    try:
+        oid = bson.ObjectId(oid)
+        return oid
+    except Exception as e:
+        raise InvalidObjectIdException(oid=oid)
