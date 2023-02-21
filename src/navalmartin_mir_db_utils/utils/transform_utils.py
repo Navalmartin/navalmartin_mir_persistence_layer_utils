85de2ab59f9ea18e12a0cfbbd4e5d7fff0aa455f
pockerman <a.giavaras@gmail.com>
2023-02-21 08:39:46 +0000

    Add transform utils

---
+++ src/navalmartin_mir_db_utils/utils/transform_utils.py
@@ -0,0 +1,12 @@
+from typing import List, Any
+
+def stringify_dictionary(data: dict, names: List[str]) -> dict:
+    for name in data:
+        if name in names:
+            data[name] = str(data[name])
+    return data
+
+
+def stringify_list(data: List[Any]) -> List[str]:
+    data = [str(item) for item in data]
+    return data
