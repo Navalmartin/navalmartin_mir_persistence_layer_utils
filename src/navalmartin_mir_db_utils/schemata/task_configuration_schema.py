fa06ca2c38b8280baf41e1aa50e3c65f492a5f67
pockerman <a.giavaras@gmail.com>
2023-03-15 18:13:38 +0000

    fix naming convention according to specification

--- src/navalmartin_mir_db_utils/schemata/task_configuration_schema.py
+++ src/navalmartin_mir_db_utils/schemata/task_configuration_schema.py
@@ -3,9 +3,9 @@ from typing import List, Dict
 
 
 class TaskConfigurationSchema(BaseModel):
-    input: List[Dict] = Field(title="input",
-                              description="The input for the task",
-                              default=[])
+    task_input: List[Dict] = Field(title="task_input",
+                                   description="The input for the task",
+                                   default=[])
     detail: List[Dict] = Field(title="detail",
                                description="Details pertaining to the task",
                                default=[])
