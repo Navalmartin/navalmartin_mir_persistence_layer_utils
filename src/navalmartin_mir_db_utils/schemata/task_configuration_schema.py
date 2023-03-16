05b78cb7dc475097306fbe61d69d4bf0e7b1a6ea
pockerman <a.giavaras@gmail.com>
2023-03-16 12:34:11 +0000

    Update schema for configuration

--- src/navalmartin_mir_db_utils/schemata/task_configuration_schema.py
+++ src/navalmartin_mir_db_utils/schemata/task_configuration_schema.py
@@ -3,7 +3,7 @@ from typing import List, Dict
 
 
 class TaskConfigurationSchema(BaseModel):
-    task_input: List[Dict] = Field(title="task_input",
+    task_input: List[Any] = Field(title="task_input",
                                    description="The input for the task",
                                    default=[])
     detail: List[Dict] = Field(title="detail",
