e48557b37aa0aaf5586f54fc66a53fcc0d21ea4e
pockerman <a.giavaras@gmail.com>
2023-03-16 12:58:13 +0000

    Fix missing import

--- src/navalmartin_mir_db_utils/schemata/task_configuration_schema.py
+++ src/navalmartin_mir_db_utils/schemata/task_configuration_schema.py
@@ -1,11 +1,11 @@
 from pydantic import BaseModel, Field
-from typing import List, Dict
+from typing import List, Dict, Any
 
 
 class TaskConfigurationSchema(BaseModel):
     task_input: List[Any] = Field(title="task_input",
-                                   description="The input for the task",
-                                   default=[])
+                                  description="The input for the task",
+                                  default=[])
     detail: List[Dict] = Field(title="detail",
                                description="Details pertaining to the task",
                                default=[])
