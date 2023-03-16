b17cbea32a3940353b430443cdb3232540920f77
pockerman <a.giavaras@gmail.com>
2023-03-16 09:35:10 +0000

    Update configuration schema

--- src/navalmartin_mir_db_utils/schemata/task_configuration_schema.py
+++ src/navalmartin_mir_db_utils/schemata/task_configuration_schema.py
@@ -9,3 +9,22 @@ class TaskConfigurationSchema(BaseModel):
     detail: List[Dict] = Field(title="detail",
                                description="Details pertaining to the task",
                                default=[])
+
+    task_packages_versions_required: Dict = Field(title="task_packages_versions_required",
+                                                  description="Specific packages versions the task requires",
+                                                  default={})
+
+    stage_task_id: int = Field(title="stage_task_id",
+                               description="The id of the stage that this task has. "
+                                           "By default it is assumed that the task is not part of any StagedTask",
+                               default=-1)
+
+    has_next_stage: bool = Field(title="has_next_stage",
+                                 description="Flag indicating if the task has a next stage. "
+                                             "By default it is assumed that the task is not part of any StagedTask",
+                                 default=False)
+
+    next_stage_id: int = Field(title="next_stage_id",
+                               description="If the task has next stage then this is id of the next stage. "
+                                           "By default it is assumed that the task is not part of any StagedTask",
+                               default=-1)
