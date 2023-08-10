79beb5fd657081dd94d22320cab211ec1b18c59e
pockerman <a.giavaras@gmail.com>
2023-08-10 07:44:41 +0100

    Update MongoDBSession class to inluce arguments

--- src/navalmartin_mir_db_utils/dbs/mongodb_session.py
+++ src/navalmartin_mir_db_utils/dbs/mongodb_session.py
@@ -10,28 +10,62 @@ from navalmartin_mir_db_utils.dbs.dbs_utils import DB_ERROR
 
 
 class MongoDBSession(object):
-    def __init__(self, mongodb_url: str = "", db_name: str = ""):
+
+    @classmethod
+    def build_with_arguments(cls, mongodb_url:str, db_name: str, **kwargs) -> "MongoDBSession":
+        """Build a MongoDBSession with provided arguments. For a list
+        of arguments check https://motor.readthedocs.io/en/stable/api-asyncio/asyncio_motor_client.html
+
+        Parameters
+        ----------
+        mongodb_url: The MongoDB URL to use
+        db_name: The database name to connect to
+        kwargs: The arguments to use
+
+        Returns
+        -------
+
+        An instance of MongoDBSession
+        """
+
+        db_session = MongoDBSession(mongodb_url=mongodb_url,
+                                    db_name=db_name,
+                                    build_client=False)
+
+        db_session.client = motor.motor_asyncio.AsyncIOMotorClient(mongodb_url, **kwargs)
+        db_session.db = db_session.client[db_name]
+        return db_session
+
+
+    def __init__(self, mongodb_url: str = "", db_name: str = "",
+                 build_client: bool = True, **kwargs: dict):
         self.mongodb_url: Union[str | None] = mongodb_url
         self.db_name: Union[str | None] = db_name
         self.client = None
         self.db = None
 
-        if self.mongodb_url == "" or self.mongodb_url is None:
-            self.mongodb_url = os.getenv("MONGODB_URL")
+        if build_client:
+            if self.mongodb_url == "" or self.mongodb_url is None:
+                self.mongodb_url = os.getenv("MONGODB_URL")
 
-        if self.mongodb_url is None or self.mongodb_url == "":
-            print(f"{DB_ERROR} The MongoDB URL is None or empty. Check your configuration")
-            raise ValueError("MongoDB URL is not set")
+            if self.mongodb_url is None or self.mongodb_url == "":
+                print(f"{DB_ERROR} The MongoDB URL is None or empty. Check your configuration")
+                raise ValueError("MongoDB URL is not set")
 
-        if self.db_name == "" or self.db_name is None:
-            self.db_name = os.getenv("MONGODB_NAME")
+            if self.db_name == "" or self.db_name is None:
+                self.db_name = os.getenv("MONGODB_NAME")
 
-        if self.db_name is None or self.db_name == "":
-            print(f"{DB_ERROR} The DB name is None or empty. Check your configuration")
-            raise ValueError("DB name is not set")
+            if self.db_name is None or self.db_name == "":
+                print(f"{DB_ERROR} The DB name is None or empty. Check your configuration")
+                raise ValueError("DB name is not set")
 
-        self.client = motor.motor_asyncio.AsyncIOMotorClient(self.mongodb_url)
-        self.db = self.client[self.db_name]
+
+            self.client = motor.motor_asyncio.AsyncIOMotorClient(self.mongodb_url, **kwargs)
+            self.db = self.client[self.db_name]
+
+    async def drop_database(self) -> None:
+        await self.client.drop_database(self.db_name)
+
 
 
 def get_db_session() -> MongoDBSession:
--- src/navalmartin_mir_db_utils/examples/simple_queries.py
+++ src/navalmartin_mir_db_utils/examples/simple_queries.py
@@ -1,69 +1,47 @@
 import asyncio
 import bson
 from navalmartin_mir_db_utils.dbs.mongodb_session import MongoDBSession
-from navalmartin_mir_db_utils.crud.mongodb_crud_ops import ReadEntityCRUDAPI
+from navalmartin_mir_db_utils.crud.mongodb_crud_ops import ReadEntityCRUDAPI, CreateEntityCRUDAPI
 from navalmartin_mir_db_utils.utils.exceptions import ResourceNotFoundException
 from navalmartin_mir_db_utils.crud.crud_utils import get_one_result_or_raise
 
-COLLECTION_NAME = "YOUR_COLLECTION_NAME"
-MONGODB_URL = "YOUR_MONGODB_URL"
-MONGO_DB_NAME_FROM = "YOUR_MONGODB_NAME"
+COLLECTION_NAME = "individuals"
+MONGODB_URL = "mongodb://localhost:27021/?replicaSet=mongo-mir-app-set"
+MONGO_DB_NAME = "MyDb"
 
+async def run_examples(mir_db_session: MongoDBSession) -> None:
 
-async def query_db(mongodb_session: MongoDBSession, criteria: dict,
-                   projection: dict,
-                   collection_name: str):
-    query_result = ReadEntityCRUDAPI.find(criteria=criteria, projection=projection,
-                                          db_session=mongodb_session,
-                                          collection_name=collection_name)
-    docs = [doc async for doc in query_result]
-    return docs
+    # insert some data
+    response = await CreateEntityCRUDAPI.insert_one(data={"name": "John", "surname": "Doe",
+                                                          "city":"SunCity",
+                                                          "address": "54 JohnDoe highway"},
+                                                    collection_name=COLLECTION_NAME,
+                                                    db_session=mir_db_session)
 
+    # get the result back
+    read_response = await ReadEntityCRUDAPI.find_one(criteria={'_id': response.inserted_id},
+                                                 collection_name=COLLECTION_NAME,
+                                                 db_session=mir_db_session,
+                                                 projection={})
 
-async def count_docs(mongodb_session: MongoDBSession, criteria: dict,
-                     collection_name: str):
-    query_result = await ReadEntityCRUDAPI.count_documents(criteria=criteria,
-                                                           db_session=mongodb_session,
-                                                           collection_name=collection_name)
-    return query_result
+    print(f"Inserted document {read_response}")
+    print("Dropping database")
+    await mir_db_session.drop_database()
 
-
-async def query_db_or_raise(mongodb_session: MongoDBSession, criteria: dict,
-                            projection: dict,
-                            collection_name: str):
-    query_result = await get_one_result_or_raise(crud_handler=ReadEntityCRUDAPI(collection_name=collection_name),
-                                                 projection=projection,
-                                                 criteria=criteria,
-                                                 db_session=mongodb_session)
-
-    return query_result
-
-
-async def run_examples(mir_db_session_from: MongoDBSession):
-    result = await query_db(mongodb_session=mir_db_session_from,
-                            criteria={'_id': bson.ObjectId('63ebc9f94c092a48bd179ae7')},
-                            projection={},
-                            collection_name=COLLECTION_NAME)
-    print(result)
-    n_docs = await count_docs(mongodb_session=mir_db_session_from,
-                              criteria={},
-                              collection_name=COLLECTION_NAME)
-    print(n_docs)
-
-    try:
-        result = await query_db_or_raise(mongodb_session=mir_db_session_from,
-                                         criteria={'survey_idx': bson.ObjectId('63ad64252c853ee163fc6a63')},
-                                         projection={'original_filename': 1},
-                                         collection_name=COLLECTION_NAME)
-    except ResourceNotFoundException as e:
-        print(str(e))
 
 
 def main():
-    mir_db_session_from = MongoDBSession(mongodb_url=MONGODB_URL,
-                                         db_name=MONGO_DB_NAME_FROM)
 
-    asyncio.run(run_examples(mir_db_session_from=mir_db_session_from))
+    mir_db_session = MongoDBSession(mongodb_url=MONGODB_URL,
+                                    db_name=MONGO_DB_NAME,
+                                    **{'directConnection': True,
+                                       'serverSelectionTimeoutMS': 5})
+
+    print(f"Session to database: {mir_db_session.db_name}")
+    print(f"Database: {mir_db_session.mongodb_url}")
+
+
+    asyncio.run(run_examples(mir_db_session=mir_db_session))
 
     
 if __name__ == '__main__':
