320d5d560d7525fbf18f16df0a3bc41250dd1b09
pockerman <a.giavaras@gmail.com>
2023-08-24 07:57:41 +0100

    Simplify schema for TaskResultSchema

--- src/navalmartin_mir_db_utils/schemata/task_result_schema.py
+++ src/navalmartin_mir_db_utils/schemata/task_result_schema.py
@@ -5,7 +5,7 @@ to be embedded in a task document specified by the client code
 """
 
 from pydantic import BaseModel, Field
-from typing import List
+from typing import List, Dict
 
 from navalmartin_mir_db_utils.utils import InvalidTypeEnum
 
@@ -26,6 +26,6 @@ class TaskResultSchema(BaseModel):
     errors: List[ErrorType] = Field(title="errors",
                                     description="The errors occurred whilst the task was running",
                                     default=[])
-    results: List[dict] = Field(title="results",
-                                description="The results obtained by running the task was running",
-                                default={})
+    results: Dict = Field(title="results",
+                          description="The results obtained by running the task was running",
+                          default={})
